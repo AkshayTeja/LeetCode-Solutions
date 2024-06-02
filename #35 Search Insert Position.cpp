b3aa3a80f2ba9cc9e923e7d043e348c72e847085
Axay17 <akshayteja04@gmail.com>
2024-06-02 15:14:56 +0530

    #35 added

---
+++ #35 Search Insert Position.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low=0,high=nums.size()-1,flag=0;
+        int mid=(low+high)/2;
+        while(low<=high)
+        {
+            if(nums[mid]==target)
+            {
+                flag=1;
+                break;
+            }
+            else if(target>nums[mid])
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+            mid=(low+high)/2;
+        }
+        if(flag==1)
+        {
+            return mid;
+        }
+        return low;
+    }
+};
