dbd0b0f67da0fc3bff3de202afd5ac45e3f02713
Axay17 <akshayteja04@gmail.com>
2024-06-06 16:39:07 +0530

    #35 another solution added

--- #35 Search Insert Position.cpp
+++ #35 Search Insert Position.cpp
@@ -27,3 +27,29 @@ public:
         return low;
     }
 };
+
+//Another solution using lower bound code
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low=0,n=nums.size(),high=n-1,mid,ans;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>=target)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;  
+    }
+};
