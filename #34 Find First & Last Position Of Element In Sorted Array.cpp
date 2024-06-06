f7c48df588b5da4e635892bc0321d98bb65661e9
Axay17 <akshayteja04@gmail.com>
2024-06-06 19:04:31 +0530

    #34 another solution added

--- #34 Find First & Last Position Of Element In Sorted Array.cpp
+++ #34 Find First & Last Position Of Element In Sorted Array.cpp
@@ -50,3 +50,63 @@ public:
         return {lb,ub-1};
     }
 };
+
+//Another solution
+class Solution {
+public:
+    int firstOccurence(vector<int>arr, int n,int target)
+    {
+        int low=0,high=n-1,mid,first=-1;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(arr[mid]==target)
+            {
+                first=mid;
+                high=mid-1;
+            }
+            else if(arr[mid]<target)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return first;
+    }
+
+    int lastOccurence(vector<int>arr, int n,int target)
+    {
+        int low=0,high=n-1,mid,last=-1;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(arr[mid]==target)
+            {
+                last=mid;
+                low=mid+1;
+            }
+            else if(arr[mid]<target)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return last;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first=firstOccurence(nums,nums.size(),target);
+        if(first==-1)
+        {
+            return {-1,-1};
+        }
+        int last=lastOccurence(nums,nums.size(),target);
+        return {first,last};
+        }
+};
