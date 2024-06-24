38bc5d2176996752db63ea3606435e0069fecd7f
Axay17 <akshayteja04@gmail.com>
2024-06-24 16:55:55 +0530

    #145 Solution added

--- #145 Binary Tree Postorder Traversal.cpp
+++ #145 Binary Tree Postorder Traversal.cpp
@@ -27,3 +27,81 @@ public:
         return ans;
     }
 };
+
+//Iterative using 2 stacks
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> ans;
+        TreeNode *node=root;
+
+        while(true)
+        {
+            if(node!=NULL)
+            {
+                st.push(node);
+                node=node->left;
+            }
+            else
+            {
+                if(st.empty())
+                    break;
+                node=st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node=node->right;
+            }
+        }
+        return ans;
+    }
+};
+
+//Iterative using one stack
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root==NULL)
+            return ans;
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+
+        while(!st1.empty())
+        {
+            root=st1.top();
+            st1.pop();
+            ans.push_back(root->val);
+            if(root->left)
+                st1.push(root->left);
+            if(root->right)
+                st1.push(root->right);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
