55e8819a9cdd7d775aa43117b6152657c2d3c14b
Axay17 <akshayteja04@gmail.com>
2024-06-29 13:00:04 +0530

    #653 Solution added

---
+++ #653 Two Sum IV - Input Is A BST.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode *root,vector<int> &inorder)
+    {
+        if(root==NULL)
+            return;
+        helper(root->left,inorder);
+        inorder.push_back(root->val);
+        helper(root->right,inorder);
+    }
+
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> inorder;
+        helper(root,inorder);
+
+        if(!root || (!root->left && !root->right))
+        {
+            return false;
+        }
+
+        int first=0;
+        int last=inorder.size()-1;
+        int flag=false;
+        while(first<last)
+        {
+            if(inorder[first]+inorder[last]<k)
+                first++;
+            else if(inorder[first]+inorder[last]>k)
+                last--;
+            else
+            {
+                flag=true;
+                break;
+            }
+        }
+        return flag;
+    }
+};
