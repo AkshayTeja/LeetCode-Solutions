5edfa4d296a069f192ace9b1cf857c157650143a
Axay17 <akshayteja04@gmail.com>
2024-07-05 16:19:12 +0530

    #210 Solution added

--- #210 Course Schedule II.cpp
+++ #210 Course Schedule II.cpp
@@ -1,3 +1,5 @@
+//Solution with DFS
+
 class Solution {
 public:
     bool dfs(vector<int> &visited,vector<int> &pathVisited,
@@ -50,5 +52,55 @@ public:
 
         reverse(ans.begin(),ans.end());
         return ans;
+    }
+};
+
+//Solution with BFS
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for (auto it:prerequisites) 
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> indegree(numCourses,0);
+        for(int i=0;i<numCourses;i++)
+        {
+            for(auto it:adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                    q.push(it);
+            }
+        }
+
+        if(ans.size()==numCourses)
+            return ans;
+        return {};
     }
 };
