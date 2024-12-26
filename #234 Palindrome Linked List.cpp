ba3cea66873603c0ae13d36b8503bc2fe2e01b24
Axay17 <akshayteja04@gmail.com>
2024-12-26 11:21:44 +0530

    #234 Solution added

--- #234 Palindrome Linked List.cpp
+++ #234 Palindrome Linked List.cpp
@@ -8,36 +8,38 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(ListNode* head) {
-        if(head==NULL || head->next==NULL)
+    bool isPalindrome(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
         {
             return true;
         }
-        int total=1,index=1;
-        ListNode *temp=head;
+        int total = 1, index = 1;
+        ListNode *temp = head;
         stack<int> st;
-        while(temp)
+        while (temp)
         {
             st.push(temp->val);
-            temp=temp->next;
+            temp = temp->next;
         }
-        temp=head;
-        while(temp)
+        temp = head;
+        while (temp)
         {
-            if(st.top()!=temp->val)
+            if (st.top() != temp->val)
             {
                 return false;
             }
             st.pop();
-            temp=temp->next;
+            temp = temp->next;
         }
         return true;
     }
 };
 
-//Another Solution
+// Another Solution
 
 /**
  * Definition for singly-linked list.
@@ -49,46 +51,81 @@ public:
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode *prev=nullptr,*current=head,*nextNode;
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *prev = nullptr, *current = head, *nextNode;
 
-        while(current)
+        while (current)
         {
-            nextNode=current->next;
-            current->next=prev;
-            prev=current;
-            current=nextNode;
+            nextNode = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextNode;
         }
-        head=prev;
+        head = prev;
         return head;
     }
 
-    bool isPalindrome(ListNode* head) {
-        if(head==NULL || head->next==NULL)
+    bool isPalindrome(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
         {
             return true;
         }
-        ListNode *slow=head,*fast=head;
-        while(fast-> next && fast->next->next)
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next)
         {
-            slow=slow->next;
-            fast=fast->next->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode *newHead=reverseList(slow->next);
-        ListNode *first=head;
-        ListNode *second=newHead;
-        while(second!=NULL)
+        ListNode *newHead = reverseList(slow->next);
+        ListNode *first = head;
+        ListNode *second = newHead;
+        while (second != NULL)
         {
-            if(first->val!=second->val)
+            if (first->val != second->val)
             {
                 return false;
             }
-            first=first->next;
-            second=second->next;
+            first = first->next;
+            second = second->next;
         }
         reverseList(newHead);
         return true;
+    }
+};
+
+// Another Solution (easiest)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        vector<int> list;
+        while (head)
+        {
+            list.push_back(head->val);
+            head = head->next;
+        }
+        int left = 0, right = list.size() - 1;
+        while (left < right && list[left] == list[right])
+        {
+            left++;
+            right--;
+        }
+        return left >= right;
     }
 };
