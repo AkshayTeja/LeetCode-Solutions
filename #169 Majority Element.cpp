4bb9863cbde482b25ac6fc5d448d843f542c068b
Axay17 <akshayteja04@gmail.com>
2025-05-21 22:52:07 +0530

    #169 Solution added

--- #169 Majority Element.cpp
+++ #169 Majority Element.cpp
@@ -1,32 +1,63 @@
-class Solution {
+class Solution
+{
 public:
-    int majorityElement(vector<int>& nums) {
-    int ele=nums[0],count=0;
-    int n=nums.size();
-    for(int i=0;i<n;i++)
+    int majorityElement(vector<int> &nums)
     {
-        if(nums[i]==ele)
-            count++;
-        else
+        int ele = nums[0], count = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
         {
-            count--; 
-            if(count<0)
+            if (nums[i] == ele)
+                count++;
+            else
             {
-                ele=nums[i];
-                count++; 
-            }        
+                count--;
+                if (count < 0)
+                {
+                    ele = nums[i];
+                    count++;
+                }
+            }
         }
-    }
-    int cnt=0;
-    for(int i=0;i<n;i++)
-    {
-        if(nums[i]==ele)
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
         {
-            cnt++;
-        }   
+            if (nums[i] == ele)
+            {
+                cnt++;
+            }
+        }
+        if (cnt > (n / 2))
+            return ele;
+        return 0;
     }
-    if(cnt>(n/2))
-        return ele;
-    return 0;
+};
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            mpp[nums[i]]++;
+        }
+        for (auto it : mpp)
+        {
+            if (it.second > (nums.size() / 2))
+                return it.first;
+        }
+        return 0;
     }
 };
