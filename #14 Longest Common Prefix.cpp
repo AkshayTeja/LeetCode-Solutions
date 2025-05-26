dafb2f9f885abce5ce3d48d21faa7c75bf209e0d
Axay17 <akshayteja04@gmail.com>
2025-05-26 18:01:31 +0530

    #14 Solution added

--- #14 Longest Common Prefix.cpp
+++ #14 Longest Common Prefix.cpp
@@ -1,27 +1,64 @@
-class Solution {
+class Solution
+{
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans="";
-        int i=0,flag=1;
-        while(i<strs[0].size())
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        string ans = "";
+        int i = 0, flag = 1;
+        while (i < strs[0].size())
         {
-            string str=strs[0].substr(0,i+1);
-            for(int j=1;j<strs.size();j++)
+            string str = strs[0].substr(0, i + 1);
+            for (int j = 1; j < strs.size(); j++)
             {
-                if(strs[j].substr(0,i+1)==str)
+                if (strs[j].substr(0, i + 1) == str)
                 {
                     continue;
                 }
-                flag=0;
+                flag = 0;
                 break;
             }
-            if(flag==1)
+            if (flag == 1)
             {
-                ans=str;
+                ans = str;
             }
             else
                 break;
-            i++;     
+            i++;
+        }
+        return ans;
+    }
+};
+
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        int flag;
+        string word = strs[0];
+        string ans = "";
+        if (strs.size() == 1)
+            return strs[0];
+        for (int i = 0; i < word.size(); i++)
+        {
+            flag = 0;
+            for (int index = 1; index < strs.size(); index++)
+            {
+                string curr = strs[index];
+                if (word[i] == curr[i])
+                {
+                    flag = 1;
+                }
+                else
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1)
+                ans += word[i];
+            else
+                break;
         }
         return ans;
     }
