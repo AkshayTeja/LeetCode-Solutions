ac41616c4812824628baa36cfe844853b3f42f7a
Axay17 <akshayteja04@gmail.com>
2025-06-03 15:41:53 +0530

    #229 Solution added

--- #229. Majority Element II.cpp
+++ #229. Majority Element II.cpp
@@ -57,3 +57,26 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    vector<int> majorityElement(vector<int> &nums)
+    {
+        map<int, int> mpp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            mpp[nums[i]]++;
+        }
+        vector<int> ans;
+        for (auto it : mpp)
+        {
+            int ele = it.first;
+            int count = it.second;
+            if (count > n / 3)
+                ans.push_back(ele);
+        }
+        return ans;
+    }
+};
