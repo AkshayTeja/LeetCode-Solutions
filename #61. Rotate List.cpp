42f7cfe850d954bfa4d273723449c67aa6e481bf
Axay17 <akshayteja04@gmail.com>
2025-06-20 23:33:50 +0530

    #61 Solution added

--- #61. Rotate List.cpp
+++ #61. Rotate List.cpp
@@ -37,3 +37,41 @@ public:
         return newHead;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *rotateRight(ListNode *head, int k)
+    {
+        ListNode *temp = head;
+        int size = 1;
+        if (!head)
+            return NULL;
+        while (temp->next)
+        {
+            temp = temp->next;
+            size++;
+        }
+        temp->next = head;
+        ListNode *newHead;
+        k = k % size;
+        temp = head;
+        for (int i = 0; i < size - k - 1; i++)
+        {
+            temp = temp->next;
+        }
+        newHead = temp->next;
+        temp->next = NULL;
+        return newHead;
+    }
+};
