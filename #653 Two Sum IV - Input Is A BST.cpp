c4d60cd14e64f9f3a704bc7917edf0415d676db9
Axay17 <akshayteja04@gmail.com>
2025-07-09 11:31:36 +0530

    #653 Solution added

--- #653 Two Sum IV - Input Is A BST.cpp
+++ #653 Two Sum IV - Input Is A BST.cpp
@@ -9,41 +9,87 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    void helper(TreeNode *root,vector<int> &inorder)
+    void helper(TreeNode *root, vector<int> &inorder)
     {
-        if(root==NULL)
+        if (root == NULL)
             return;
-        helper(root->left,inorder);
+        helper(root->left, inorder);
         inorder.push_back(root->val);
-        helper(root->right,inorder);
+        helper(root->right, inorder);
     }
 
-    bool findTarget(TreeNode* root, int k) {
+    bool findTarget(TreeNode *root, int k)
+    {
         vector<int> inorder;
-        helper(root,inorder);
+        helper(root, inorder);
 
-        if(!root || (!root->left && !root->right))
+        if (!root || (!root->left && !root->right))
         {
             return false;
         }
 
-        int first=0;
-        int last=inorder.size()-1;
-        int flag=false;
-        while(first<last)
+        int first = 0;
+        int last = inorder.size() - 1;
+        int flag = false;
+        while (first < last)
         {
-            if(inorder[first]+inorder[last]<k)
+            if (inorder[first] + inorder[last] < k)
                 first++;
-            else if(inorder[first]+inorder[last]>k)
+            else if (inorder[first] + inorder[last] > k)
                 last--;
             else
             {
-                flag=true;
+                flag = true;
                 break;
             }
         }
         return flag;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void helper(TreeNode *root, vector<int> &inorder)
+    {
+        if (root == NULL)
+            return;
+        helper(root->left, inorder);
+        inorder.push_back(root->val);
+        helper(root->right, inorder);
+    }
+    bool findTarget(TreeNode *root, int k)
+    {
+        vector<int> inorder;
+        helper(root, inorder);
+        if (!root || (!root->right && !root->left))
+            return false;
+        int left = 0, right = inorder.size() - 1;
+        while (left < right)
+        {
+            if (inorder[left] + inorder[right] == k)
+            {
+                return true;
+            }
+            else if (inorder[left] + inorder[right] > k)
+                right--;
+            else
+                left++;
+        }
+        return false;
     }
 };
