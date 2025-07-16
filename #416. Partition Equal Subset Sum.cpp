cf64d4161d0c6c2f1fedb774828aa62cd0d65b0c
Axay17 <akshayteja04@gmail.com>
2025-07-16 19:51:11 +0530

    #416 Solution added

---
+++ #416. Partition Equal Subset Sum.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    bool canPartition(vector<int> &nums)
+    {
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+        }
+        if (sum % 2 != 0)
+        {
+            return false;
+        }
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for (auto num : nums)
+        {
+            for (int j = target; j >= num; j--)
+            {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        return dp[target];
+    }
+};
